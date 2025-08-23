9665806e66208109aae935a1b6cc09c5f774d650
ngocthe.bui <nthe.bui@gmail.com>
2025-08-23 21:45:15 +0700

    feat: product register dto

--- src/main/java/com/custom/ngow/shop/dto/ProductRegistration.java
+++ src/main/java/com/custom/ngow/shop/dto/ProductRegistration.java
@@ -8,6 +8,7 @@ import com.custom.ngow.shop.entity.ProductColor;
 import com.custom.ngow.shop.entity.ProductSize;
 
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
@@ -34,20 +35,46 @@ public class ProductRegistration {
   @NotNull(message = "{error.required}")
   private BigDecimal salePrice;
 
+  @NotEmpty(message = "{error.required}")
   private String material; // Chất liệu
+
+  @NotNull(message = "{error.required}")
   private Integer strapQuantity; // Số lượng dây đeo
+
+  @NotEmpty(message = "{error.required}")
   private String innerPocket; // Ngăn trong
+
+  @NotEmpty(message = "{error.required}")
   private String handleLength; // Chiều dài tay cầm
+
   private Boolean removableStrap; // Dây đeo có thể tháo rời
+
   private Boolean adjustableStrap; // Dây đeo có thể điều chỉnh
+
+  @NotEmpty(message = "{error.required}")
   private String lockType; // Loại khóa cài
+
+  @NotEmpty(message = "{error.required}")
   private String strapLength; // Chiều dài dây đeo
+
+  @NotEmpty(message = "{error.required}")
   private String strapTotalLength; // Tổng chiều dài dây
+
+  @NotEmpty(message = "{error.required}")
   private String weight; // Trọng lượng
+
+  @NotEmpty(message = "{error.required}")
   private String width; // Chiều rộng
+
+  @NotEmpty(message = "{error.required}")
   private String depth; // Chiều sâu
+
+  @NotEmpty(message = "{error.required}")
   private String height; // Chiều cao
 
-  private List<ProductColor> colors = new ArrayList<>(List.of(new ProductColor()));
+  @NotEmpty(message = "{error.required}")
+  private List<ProductColor> colors = new ArrayList<>();
+
+  @NotEmpty(message = "{error.required}")
   private List<ProductSize> sizes = new ArrayList<>();
 }
--- src/main/java/com/custom/ngow/shop/page/admin/ProductController.java
+++ src/main/java/com/custom/ngow/shop/page/admin/ProductController.java
@@ -1,13 +1,9 @@
 package com.custom.ngow.shop.page.admin;
 
-import java.util.ArrayList;
-import java.util.List;
-
 import org.springframework.stereotype.Controller;
 import org.springframework.stereotype.Service;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
-import org.springframework.validation.ObjectError;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -54,21 +50,10 @@ public class ProductController {
       model.addAttribute("mode", "Create");
       model.addAttribute("adminDto", userService.getCurrentUserDto());
       model.addAttribute("productDto", productDto);
-
-      handleAllErrorsMessage(bindingResult, model);
       return "view/admin/product";
     }
     redirectAttributes.addFlashAttribute(
         "successMessage", messageUtil.getMessage("success.created", new String[] {"product"}));
     return "redirect:/admin/products/update";
-  }
-
-  private void handleAllErrorsMessage(BindingResult bindingResult, Model model) {
-    List<String> allErrorsMessage = new ArrayList<>();
-    for (ObjectError error : bindingResult.getAllErrors()) {
-      allErrorsMessage.add(error.getDefaultMessage());
-    }
-
-    model.addAttribute("allErrorsMessage", allErrorsMessage);
   }
 }
